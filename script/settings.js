eede525f323b1c6877c8dd2f1b488e8567facd1a
samneuteboom <6040120@mborijnland.nl>
2024-10-18 14:32:03 +0200

    h

---
+++ script/settings.js
@@ -0,0 +1,9 @@
+document.getElementById('myElement').style.opacity = '0,7';
+
+// Functie om het volume in te stellen
+function setVolume(value) {
+    document.getElementById('volumeValue').innerText = value;
+    // Logica om het spelvolume aan te passen
+    console.log("Volume ingesteld op: " + value);
+    // Voeg hier je game audio-logica toe, bijvoorbeeld aanpassen van audio-API.
+}
